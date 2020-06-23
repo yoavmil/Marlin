9bbab2dc35a821cc19a97d30f73aaffc2220ef7d
yoav_at_eli's <yoavm@magnusmetal.com>
2020-06-23 12:01:49 +0300

    reverse y homing direction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -613,10 +613,10 @@
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
 #define USE_XMIN_PLUG
-#define USE_YMIN_PLUG
+//#define USE_YMIN_PLUG
 #define USE_ZMIN_PLUG
 //#define USE_XMAX_PLUG
-//#define USE_YMAX_PLUG
+#define USE_YMAX_PLUG
 //#define USE_ZMAX_PLUG
 
 // Enable pullup for all endstops to prevent a floating state
@@ -1086,14 +1086,14 @@
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
 #define X_HOME_DIR -1
-#define Y_HOME_DIR -1
+#define Y_HOME_DIR +1
 #define Z_HOME_DIR -1
 
 // @section machine
 
 // The size of the print bed
 #define X_BED_SIZE 197
-#define Y_BED_SIZE 380
+#define Y_BED_SIZE 172
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
